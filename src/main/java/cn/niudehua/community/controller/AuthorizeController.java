11d87dadd8d7393598536dcf8a15ae40a1cf880b
niudehua <657563945@qq.com>
2020-04-27 02:06:08 +0800

    GitHub登录 配置文件application

--- src/main/java/cn/niudehua/community/controller/AuthorizeController.java
+++ src/main/java/cn/niudehua/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import cn.niudehua.community.dto.AccessTokenDTO;
 import cn.niudehua.community.dto.GitHubUser;
 import cn.niudehua.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +19,23 @@ public class AuthorizeController {
     @Autowired
     GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("1cdd13b1b9be4b550480");
-        accessTokenDTO.setClient_secret("255caef8a7232b3014b977f388a731576b87f95f");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = githubProvider.getGitHubUser(accessToken);
