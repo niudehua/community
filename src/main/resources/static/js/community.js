aba7ba0cfa9a4c97802e30b428eb0bcb61b0bf33
niudehua <657563945@qq.com>
2020-05-12 00:50:33 +0800

    完成二级评论功能的页面展示

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,8 +2,8 @@
  * 提交问题评论
  */
 function post() {
-    var questionId = $("#question_id").val();
-    var content = $("#comment_content").val();
+    const questionId = $("#question_id").val();
+    const content = $("#comment_content").val();
     comment2target(questionId, content, 1)
 }
 
@@ -12,8 +12,8 @@ function post() {
  * @param e
  */
 function comment(e) {
-    var commentId = e.getAttribute("data-id");
-    var content = $("#input-" + commentId).val();
+    const commentId = e.getAttribute("data-id");
+    const content = $("#input-" + commentId).val();
     comment2target(commentId, content, 2)
 
 }
@@ -44,7 +44,7 @@ function comment2target(targetId, content, type) {
                 window.location.reload();
                 // $("#comment_section").hide()
             } else if (response.code === 2003) {
-                var isAccepted = confirm(response.message);
+                const isAccepted = confirm(response.message);
                 if (isAccepted) {
                     //跳转登录页面
                     window.open("https://github.com/login/oauth/authorize?client_id=1cdd13b1b9be4b550480&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
@@ -63,11 +63,51 @@ function comment2target(targetId, content, type) {
  * 展开二级回复
  */
 function collapseComment(e) {
-    var id = e.getAttribute("data-id");
-    $.getJSON("/comment/"+id, function(data){
-        console.log(data);
-});
-    if (("#comment-" + id))
-    $("#comment-" + id).toggleClass("in");
-    $("#sub_comment_icon-" + id).toggleClass("sub-comment-icon-active");
+    const id = e.getAttribute("data-id");
+    const subCommentContainer = $("#comment-" + id);
+
+    if (subCommentContainer.hasClass("in")) {
+        subCommentContainer.toggleClass("in");
+        $("#sub_comment_icon-" + id).toggleClass("sub-comment-icon-active");
+    } else {
+        if (subCommentContainer.children().length === 1) {
+            $.getJSON("/comment/" + id, function (data) {
+                $.each(data.data.reverse(), function (index, comment) {
+
+                    const mediaLeftElement = $("<div/>", {
+                        "class": "media-left"
+                    }).append($("<img alt='网络头像' src='#'/>", {
+                        "class": "media-object img_avatar_url",
+                        "src": comment.user.avatarUrl
+                    }));
+
+                    const mediaBodyElement = $("<div/>", {
+                        "class": "media-body"
+                    }).append($("<h5/>", {
+                        "class": "text-desc",
+                        "html": comment.user.name
+                    })).append($("<div/>", {
+                        "html": comment.content
+                    })).append($("<span/>", {
+                        "class": "pull-right text-desc",
+                        "html": moment(comment.gmtCreate).format('YYYY-MM-DD')
+                    }));
+
+                    const mediaElement = $("<div/>", {
+                        "class": "media"
+                    }).append(mediaLeftElement).append(mediaBodyElement);
+
+                    subCommentContainer.prepend(mediaElement);
+                });
+
+            });
+            subCommentContainer.toggleClass("in");
+            $("#sub_comment_icon-" + id).toggleClass("sub-comment-icon-active");
+        } else {
+            subCommentContainer.toggleClass("in");
+            $("#sub_comment_icon-" + id).toggleClass("sub-comment-icon-active");
+        }
+
+    }
 }
+
